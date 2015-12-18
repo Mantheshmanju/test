4de466b87d5bd27de8dd22377a08a8eb6bcdde3f
Jordan Max <jordan.max@loop.colum.edu>
2015-12-18 16:28:22 -0600

    'basic'

--- client/app/student-profile/student-profile.controller.js
+++ client/app/student-profile/student-profile.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('alwaysHiredApp')
-  .controller('StudentProfileCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('StudentProfileCtrl', function ($scope, $rootScope) {
+    $rootScope.showNav = true;
+    //this will need to be involved, we need to check where the user is at in their profile and navigate      them to the correct tab. ie. if basic info is complete, automatically set the tab to the next tab at      which a field is incomplete.
+    
+    //also write logic for displaying certain directives when a tab is active
   });
